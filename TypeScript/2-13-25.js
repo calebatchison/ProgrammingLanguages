67febf7276edc9c4d2740ce19e6ad735e41c6dcc
SongOfSolar <c.atchison551@gmail.com>
2025-02-13 17:25:04 -0500

    class problems

---
+++ TypeScript/2-13-25.js
@@ -0,0 +1,22 @@
+// Triangle numbers
+function triangleNumbers(n) {
+    var sum = 0;
+    for (var i = 1; i <= n; i++) {
+        sum += i;
+    }
+}
+function factorsOf(n) {
+    var factors = [];
+    for (var i = 1; i <= n; i++) {
+        if (n % i === 0) {
+            factors.push(i);
+        }
+    }
+    return factors;
+}
+function solveProblem1(n) {
+    var triangleNum = triangleNumbers(n);
+    var factors = factorsOf(triangleNum);
+    return factors.length;
+}
+console.log(solveProblem1(100));
